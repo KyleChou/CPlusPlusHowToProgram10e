38e8b0131414a0c9cc2fb692b3154a1652802af3
kylechou <kylechou94@gmail.com>
2020-03-18 10:33:18 +0800

    update

---
+++ examples/ch02/fig2.3.cpp
@@ -0,0 +1,9 @@
+// Fig. 2.3: fig2.3.cpp
+// Printing a line of text with multiple statements.
+#include <iostream> // enables program to output data to the screen
+
+// function main begins program execution
+int main() {
+    std::cout << "Welcome ";
+    std::cout << "to C++!\n";
+} // end function main
